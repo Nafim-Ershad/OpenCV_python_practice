d07149cb2d37391bfa8622c77939349cb81d7a11
Nafim Ershad <inan.nafim1089@outlook.com>
2026-05-02 23:32:39 +0600

    First commit with some code and README.md

---
+++ Introduction/hello_world.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+import numpy as np
+
+if __name__ == "__main__":
+    
+    print("OpenCV:", cv.__version__)
+    img = np.zeros((120, 400, 3), dtype=np.uint8)
+    cv.putText(img, "Hello World!!", (10, 80), cv.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 3)
+    # If you installed a non-headless build, you can display a window:
+    # cv.imshow("hello", img); cv.waitKey(0)
+    # Always safe (headless or not): save to file
+    cv.imwrite("./assets/hello.png", img)
---
+++ README.md
@@ -0,0 +1,6 @@
+# Computer Vision Learning Repository
+
+This repository contains my study progress, and OpenCV practice work for learning fundamental concepts in Computer Vision.
+
+The goal of this repository is to build both theoretical understanding and practical implementation skills in topics such as image processing, image stitching, camera calibration, stereo vision, optical flow, structure from motion, and object tracking etc.
+
