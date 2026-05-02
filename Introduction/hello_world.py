f4bf77feee0be3ee9b4b6d723b5cbfb75e268574
Nafim Ershad <inan.nafim1089@outlook.com>
2026-05-03 02:58:08 +0600

    Added some comments

--- Introduction/hello_world.py
+++ Introduction/hello_world.py
@@ -4,9 +4,13 @@ import numpy as np
 if __name__ == "__main__":
     
     print("OpenCV:", cv.__version__)
-    img = np.zeros((120, 400, 3), dtype=np.uint8)
-    cv.putText(img, "Hello World!!", (10, 80), cv.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 3)
-    # If you installed a non-headless build, you can display a window:
-    # cv.imshow("hello", img); cv.waitKey(0)
-    # Always safe (headless or not): save to file
-    cv.imwrite("./assets/hello.png", img)
+    
+    img = np.zeros((120, 400, 3), dtype=np.uint8) # Generate a black image
+
+    cv.putText(img, "Hello World!!", (10, 80), cv.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 3) # Write "Hello World!!" on the image
+    # image_instance, text, position, font, font_scale, color, thickness
+    
+    cv.imwrite("./assets/hello.png", img)   # Save the image to the specified path
+    cv.imshow("Hello World", img)           # Display the image in a window
+    if cv.waitKey(0) & 0xFF == ord('q'):    # Wait for a key press
+        cv.destroyAllWindows()             # Close all windows
